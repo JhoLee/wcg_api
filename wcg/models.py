36af573c4180fc3d8ce9c10b3656b46b8d8251b8
Joo-Ho Lee <jooho_lee@outlook.kr>
2019-05-08 15:12:53 +0900

    ENH: 이미지 업로드 가능하도록 모델 추가

--- wcg/models.py
+++ wcg/models.py
@@ -1,3 +1,41 @@
-from django.db import models
+from time import strftime
 
-# Create your models here.
+from django.db import models
+from colorfield.fields import ColorField
+
+
+def user_path(instance, filename):
+    from random import choice
+    import string
+
+    arr = [choice(string.ascii_letters) for _ in range(8)]
+    pid = ''.join(arr)
+    extension = filename.split('.')[-1]
+
+    return 'mask_image/%s.%s' % (pid, extension)
+
+
+def mask_path(instance, filename):
+    """
+
+    :param instance: instance.
+    :param filename: maks image's file name
+    :return: mask image's path to save in server.
+    """
+    # TODO: Check file's extension.
+
+    date = strftime("%y%m%d")
+    time = strftime("%H%M%S")
+    extension = filename.split('.')[-1]
+
+    path = "%s_%s_%s_%s.%s" % (instance.title, instance.font, date, time, extension)
+
+    return path
+
+
+class WordCloud(models.Model):
+    title = models.CharField(max_length=100)
+    mask_image = models.ImageField(upload_to=mask_path)
+    font = models.CharField(max_length=50)
+    data = models.TextField()
+    background_color = ColorField(default="#FFFFFF")
