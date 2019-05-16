8ea2c200cbad032e40c8889d61bf4ab28d32d591
Joo-Ho Lee <jooho_lee@outlook.kr>
2019-05-16 11:31:24 +0900

    CLN: Rename 'Request' to 'Order'

--- wcg/models.py
+++ wcg/models.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
 from pytz import timezone
-from time import strftime
 
 from colorfield.fields import ColorField
 from django.db import models
@@ -44,19 +43,19 @@ class Font(models.Model):
     font_file = models.FileField(upload_to=font_path)
 
 
-class Request(models.Model):
+class Order(models.Model):
     def __str__(self):
-        return "{requester}-{title}-{date}_{time}".format(
-            requester=self.requester,
+        return "{client}-{title}-{date}_{time}".format(
+            client=self.client,
             title=self.title,
-            date=datetime.strftime(self.requested_at_korean_time, "%y%m%d"),
-            time=datetime.strftime(self.requested_at_korean_time, "%H%M%S")
+            date=datetime.strftime(self.ordered_at_korean_time, "%y%m%d"),
+            time=datetime.strftime(self.ordered_at_korean_time, "%H%M%S")
         )
 
     @property
-    def requested_at_korean_time(self):
+    def ordered_at_korean_time(self):
         korean_timezone = timezone(settings.TIME_ZONE)
-        return self.requested_at.astimezone(korean_timezone)
+        return self.ordered_at.astimezone(korean_timezone)
 
     def mask_image_path(self, filename):
         """
@@ -66,8 +65,8 @@ class Request(models.Model):
         :return: mask image's path to save in server.
         """
 
-        date = datetime.strftime(self.requested_at_korean_time, "%y%m%d")
-        time = datetime.strftime(self.requested_at_korean_time, "%H%M%S")
+        date = datetime.strftime(self.ordered_at_korean_time, "%y%m%d")
+        time = datetime.strftime(self.ordered_at_korean_time, "%H%M%S")
         extension = filename.split('.')[-1]
 
         name = "{date}/{title}_{date}_{time}.{extension}".format(
@@ -78,31 +77,32 @@ class Request(models.Model):
         )
         return media_path("mask_image", name)
 
-    requester = models.CharField(max_length=200, default="unknown")
+    client = models.CharField(max_length=200, default="unknown")
     title = models.CharField(max_length=100)
     data = models.TextField(default="")
     font = models.ForeignKey(Font, models.SET_DEFAULT, default=None)
     mask_image = models.ImageField(upload_to=mask_image_path, blank=True)
     background_color = ColorField(default="#FFFFFF")
-    requested_at = models.DateTimeField(auto_now_add=True)
+    ordered_at = models.DateTimeField(auto_now_add=True)
 
 
 class WordCloud(models.Model):
     def __str__(self):
-        return self.request.title + "_" + str(datetime.strftime(self.request.requested_at_korean_time, "%y%m%d_%H%M%S"))
+        return self.order.title + "_" + str(datetime.strftime(self.order.oredered_at_korean_time, "%y%m%d_%H%M%S"))
 
     def word_cloud_path(self, filename):
-        date = datetime.strftime(self.request.requested_at_korean_time, "%y%m%d")
-        time = datetime.strftime(self.request.requested_at_korean_time, "%H%M%S")
+        date = datetime.strftime(self.order.oredered_at_korean_time, "%y%m%d")
+        time = datetime.strftime(self.order.ordered_at_korean_time, "%H%M%S")
         extension = filename.split('.')[-1]
+
         name = "{date}/{title}_{date}_{time}.{extension}".format(
             date=date,
-            title=self.request.title,
+            title=self.order.title,
             time=time,
             extension=extension
         )
 
         return media_path("mask_image", name)
 
-    request = models.ForeignKey(Request, models.CASCADE)
+    order = models.ForeignKey(Order, models.CASCADE)
     wordCloud = models.FileField(upload_to=word_cloud_path, blank=True)
